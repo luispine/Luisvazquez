232518f12a13e09a5cee43e71fde7cf4f0c4539c
luis vzquez <106345552+luispine@users.noreply.github.com>
2023-09-07 20:20:35 -0600

    Header Closes #1 (#4)
    * se suben archivos a header #1
    
    * agragamos el commit #1
    
    * guardo cambios echos
    
    * se corrigen pruebas #1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../servises/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private headerService : HeaderService){
+
+  }
+
+  name:String='fakename'
+  charges:String='whatever'
+  objetive:String='fake'
+  image:String=''
+  email:String='noone'
+  cel:String='0000'
+  ubication:String='here'
+  social:String='type'
+  
+
+  ngOnInit() : void {
+    this.headerService.getHeader()
+    .subscribe((date: any) => {
+      this.name=date.name
+      this.charges=date.charges
+      this.objetive=date.objetive
+      this.image=date.photo
+      this.email=date.email
+      this.cel=date.cel
+      this.ubication=date.ubication
+      this.social=date.social
+    })
+  }
 
 }
---
+++ src/app/servises/header.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError} from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+  apiURL = 'http://localhost:8080/header'
+  constructor(private http: HttpClient)
+  { }
+
+  httpOptions = {
+	  headers : new HttpHeaders({
+		  'contendt-Type': 'application/jason'
+	  })
+  }
+
+  getHeader():Observable<any>{
+	  return this .http.get<any>(this.apiURL, this.httpOptions)
+	  .pipe(
+		  retry(1),
+		  catchError(this.handleError)
+	  )
+  }
+  handleError(error: any){
+	  let errorMessage = '';
+
+	  if (error.error instanceof ErrorEvent){
+		// Get Client.side error
+		errorMessage = error.error.massage;
+	  }else{
+		// Get server-side error
+		errorMessage = `Error code: ${error.status}\n Messenger: ${error.massege}`;
+	  }
+
+	  window.alert(errorMessage);
+	  return throwError(errorMessage);
+  }
+}
